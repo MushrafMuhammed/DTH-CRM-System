0680a6d331406f474b7498fbc4ea836fe9fbc1df
Mushraf Muhammed <mushrafmuhammed@outlook.com>
2024-01-17 15:58:37 +0530

    adminapp-templates

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -1,5 +1,28 @@
 from django.urls import path
+from adminapp import views
+import adminapp
+
+app_name = 'adminapp'
 
 urlpatterns = [
-   
+   path('login/',  views.loginfun, name='login'),
+   path('register/',  views.registerfun, name='register'),
+   path('',  views.dashboardfun, name='dashboard'),
+   path('dashboard/', views.dashboardfun, name='dashboard'),
+   path('customer/', views.customerfun, name='customers'),
+   path('people/', views.peoplefun, name='people'),
+   path('company/', views.companyfun, name='companies'),
+   path('lead/', views.leadfun, name='leads'),
+   path('offer/', views.offerfun, name='offers'),
+   path('invoice/', views.invoicefun, name='invoice'),
+   path('quote/', views.quotefun, name='quote'),
+   path('payment/', views.paymentfun, name='payment'),
+   path('expenses/', views.expensesfun, name='expense'),
+   path('product/', views.productfun, name='product'),
+   path('product_category/', views.category_productfun, name='category_product'),
+   # path('layout/', views.layoutfun, name='layout'),
+   # path('test/', views.testfun, name='test'),
+
+
+
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,3 +1,56 @@
+from django.shortcuts import render
 
+# Create your views here.
+
+def loginfun(request):
+    return render(request, 'adminapp/pages-login.html')
+
+def registerfun(request):
+    return render(request, 'adminapp/pages-register.html')
+
+def dashboardfun(request):
+    return render(request, 'adminapp/pages-dashboard.html')
+
+def customerfun(request):
+    return render(request, 'adminapp/pages-customer.html')
+
+def peoplefun(request):
+    return render(request, 'adminapp/pages-people.html')
+
+def companyfun(request):
+    return render(request, 'adminapp/pages-company.html')
+
+def leadfun(request):
+    return render(request, 'adminapp/pages-lead.html')
+
+def offerfun(request):
+    return render(request, 'adminapp/pages-offer.html')
+
+
+def invoicefun(request):
+    return render(request, 'adminapp/pages-invoice.html')
+
+def quotefun(request):
+    return render(request, 'adminapp/pages-quote.html')
+
+def paymentfun(request):
+    return render(request, 'adminapp/pages-payment.html')
+
+def expensesfun(request):
+    return render(request, 'adminapp/pages-expense.html')
+
+def productfun(request):
+    return render(request, 'adminapp/pages-product.html')
+
+def category_productfun(request):
+    return render(request, 'adminapp/pages-product_category.html')
+
+
+def layoutfun(request):
+    return render(request, 'adminapp/components-layout.html')
+
+
+def testfun(request):
+    return render(request, 'adminapp/check-layout.html')
 
 
